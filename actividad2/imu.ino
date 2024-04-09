5ee4eb327f6be740a72be1dba891851c16e7cdcd
fran855 <65423343+fran855@users.noreply.github.com>
2024-04-09 00:16:24 -0300

    Update imu.ino

--- actividad2/imu.ino
+++ actividad2/imu.ino
@@ -3,11 +3,26 @@
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
+
 uint16_t tiempo_inicial = 0;
 uint16_t tiempo_final = 0;
-long int posicion_actual = 0;
+uint16_t prev_time = 0;
+uint16_t current_time = 0;
 
-const long int periodo_lectura = 100;
+float offset_giro_x = -0.04;
+float offset_giro_y = 0.03;
+float offset_giro_z = 0;
+
+float offset_accel_x = 1.13;
+float offset_accel_y = 0.39;
+float offset_accel_z = -0.75;
+
+float dt = 0;
+
+float angulo_gir_x = 0;
+float angulo_accel_x = 0;
+
+const long int periodo_lectura = 10000;
 
 void setup(void) {
 	Serial.begin(115200);
@@ -20,50 +35,49 @@ void setup(void) {
 		}
 	}
 	Serial.println("MPU6050 Found!");
-
-	// set accelerometer range to +-8G
-	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-
-	// set gyro range to +- 500 deg/s
-	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-
-	// set filter bandwidth to 5-10-21-44-94-184-260 Hz
-	mpu.setFilterBandwidth(MPU6050_BAND_10_HZ);
+	
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G); // set accelerometer range to +-8G
+	mpu.setGyroRange(MPU6050_RANGE_500_DEG);      // set gyro range to +- 500 deg/s
+	mpu.setFilterBandwidth(MPU6050_BAND_10_HZ);   // set filter bandwidth to 5-10-21-44-94-184-260 Hz
 
 	delay(100);
 }
 
+// Qué pasa si un timer desborda y el otro no?
 void loop() {
-	
-  tiempo_inicial = millis();
-  // float d1=0, d2=0, d3=0;
-  // sensors_event_t a, g, temp;
-  // mpu.getEvent(&a, &g, &temp);
+  tiempo_inicial = micros();
   
-  obtener_angulo_giroscopo();
-  Serial.print("Est: ");
-  Serial.println(posicion_actual);
-  // Serial.println(g);
-  tiempo_final = millis();
-
-  delay(periodo_lectura - (tiempo_final - tiempo_inicial));
+  current_time = micros();
+  dt = (current_time - prev_time) / 1e6;
+  obtener_angulo_accel_x();
+  prev_time = current_time;
   
 
+  tiempo_final = micros();
+  delayMicroseconds(periodo_lectura - (tiempo_final - tiempo_inicial));
 }
 
-void obtener_angulo_giroscopo(){
+void obtener_angulo_giroscopo_x() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  posicion_actual = posicion_actual + g.gyro.x * periodo_lectura; 
-  Serial.println(g.gyro.x);
+  
+  angulo_gir_x = angulo_gir_x + (g.gyro.x-offset_giro_x) * dt * 180 / M_PI; 
+
+  Serial.print("Ángulo x: ");
+  Serial.println(angulo_gir_x);
 }
 
-void matlab_send(float dato1, float dato2, float dato3){
-  Serial.write("abcd");
-  byte * b = (byte *) &dato1;
-  Serial.write(b,4);
-  b = (byte *) &dato2;
-  Serial.write(b,4);
-  b = (byte *) &dato3;
-  Serial.write(b,4);
+void obtener_angulo_accel_x() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("Accel y: ");
+  Serial.println(a.acceleration.y);
+  Serial.print("Accel z: ");
+  Serial.println(a.acceleration.z);
+
+  angulo_accel_x  = atan2(a.acceleration.y-offset_accel_y, a.acceleration.z-offset_accel_z) * 180 / M_PI;
+
+  Serial.print("Ángulo x: ");
+  Serial.println(angulo_accel_x);
 }
